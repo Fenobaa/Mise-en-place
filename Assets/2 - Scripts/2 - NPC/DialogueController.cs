512740cf6c76553c547432565b875630abe868b7
Sharli <130414200+Fenobaa@users.noreply.github.com>
2024-12-02 20:17:19 -0300

    V0.05
    Ordenar los pedidos siguiendo la image
    Cuando se elimine una orden y están en su maximo pueda spawnear otra
    Resolver error que el emplatado no detecta los fideos
    Dialogos cambian cada minuto - EN LA TEORIA FUNCIONA, FALTA PROBAR CON AUDIOS

--- Assets/2 - Scripts/2 - NPC/DialogueController.cs
+++ Assets/2 - Scripts/2 - NPC/DialogueController.cs
@@ -12,6 +12,7 @@ public class Dialogue
 {
     public string dialogueText;
     public AudioClip dialogueSound;
+
 }
 public class DialogueController : MonoBehaviour
 {
@@ -19,6 +20,8 @@ public class DialogueController : MonoBehaviour
     public TMP_Text DialogueTMP;
     public Image dialogueBackground;
     [SerializeField] private List<Dialogue> dialogue;
+    private int dialogueCounter;
+    
     
     //Dialogue Detection
     private bool PlayerIsOnArea;
@@ -27,21 +30,79 @@ public class DialogueController : MonoBehaviour
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        StartCoroutine(SecondsCounter());
     }
 
     private void Update()
     {
         if (PlayerIsOnArea && Input.GetKeyDown(KeyCode.E))
         {
-            RandomIndex = Random.Range(0, dialogue.Count);
-            DialogueTMP.text = dialogue[RandomIndex].dialogueText;
-            audioSource.clip = dialogue[RandomIndex].dialogueSound;
+            DialogueTimeController();
 
-            audioSource.Play();
         }
     }
 
+    IEnumerator SecondsCounter()
+    {
+        while (true) // Bucle infinito
+        {
+            yield return new WaitForSeconds(60); // Espera 60 segundos
+            dialogueCounter++; // Incrementa el contador
+        }
+    }
+    public void DialogueTimeController()
+    {
+        if (dialogueCounter == 0)
+        {
+            DialogueTMP.text = dialogue[0].dialogueText;
+            audioSource.clip = dialogue[0].dialogueSound;
 
+            audioSource.Play();
+        }
+        else if (dialogueCounter == 1)
+        {
+            DialogueTMP.text = dialogue[1].dialogueText;
+            audioSource.clip = dialogue[1].dialogueSound;
+
+            audioSource.Play();
+        }
+        else if (dialogueCounter == 2)
+        {
+            DialogueTMP.text = dialogue[2].dialogueText;
+            audioSource.clip = dialogue[2].dialogueSound;
+
+            audioSource.Play();   
+        }
+        else if (dialogueCounter == 3)
+        {
+            DialogueTMP.text = dialogue[3].dialogueText;
+            audioSource.clip = dialogue[3].dialogueSound;
+
+            audioSource.Play();
+        }
+        else if (dialogueCounter == 4)
+        {
+            DialogueTMP.text = dialogue[4].dialogueText;
+            audioSource.clip = dialogue[4].dialogueSound;
+
+            audioSource.Play();
+        }
+        else if (dialogueCounter == 5)
+        {
+            DialogueTMP.text = dialogue[5].dialogueText;
+            audioSource.clip = dialogue[5].dialogueSound;
+
+            audioSource.Play();
+        }
+        else if (dialogueCounter == 6)
+        {
+            DialogueTMP.text = dialogue[6].dialogueText;
+            audioSource.clip = dialogue[6].dialogueSound;
+
+            audioSource.Play();
+        }
+  
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
