e1c4c8187d549e499cca412ca6445d3058d45086
Sharli <130414200+Fenobaa@users.noreply.github.com>
2024-12-02 01:01:12 -0300

    V0.04
    Arreglado el emplatado, ahora se destruyen los ingredientes y la orden de la ui, además solo acepta ingredientes cocinados

--- Assets/2 - Scripts/4 - Ordenes/Inventory.cs
+++ Assets/2 - Scripts/4 - Ordenes/Inventory.cs
@@ -9,8 +9,8 @@ public class Inventory : MonoBehaviour
 {
     public static Inventory instance;
     public List<GameObject> panelesPedidos;
-    public PlatosScriptable[] platos = new PlatosScriptable[5];
-    private PlatosScriptable[] pedidos = new PlatosScriptable[5];
+    public PlatosScriptable[] platos = new PlatosScriptable[4];
+    private PlatosScriptable[] pedidos = new PlatosScriptable[4];
     public GameObject platoPanelPB;
     public RectTransform pedidosPanel;
 
